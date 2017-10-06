c9c951045d04eaa1a2323d2cfccb4a1003cb4605
Zach Orlovsky <sadorlovsky@gmail.com>
2017-10-06 09:26:32 +0300

    simplify

--- pages/me.js
+++ pages/me.js
@@ -1,7 +1,8 @@
 import shuffle from 'array-shuffle'
 import differenceInYears from 'date-fns/difference_in_years'
+import { Keyframes, Frame } from 'react-keyframes'
+import NoSSR from 'react-no-ssr'
 import Page from '../layouts/Page'
-import ThingySlider from '../components/ThingySlider'
 import HomeButton from '../components/HomeButton'
 
 export default () => (
@@ -11,15 +12,29 @@ export default () => (
       <div>{differenceInYears(new Date(), new Date(1994, 2, 26))} years old</div>
       <div>from Moscow, Russia</div>
       <div>can do <code>alert()</code> stuff</div>
-      <div style={{ maxHeight: '20px' }}>
-        <ThingySlider things={shuffle([
-          ['react lover', '/static/react.svg'],
-          ['know about monads', '/static/haskell.svg'],
-          ['sad boy', '/static/sad-face.svg'],
-          ['happy  devices user'],
-          ['Ph.D. student', '/static/master.svg']
-        ])} />
-      </div>
+      <NoSSR>
+        <div>
+          <Keyframes loop className='thingy-container'>
+            {shuffle([
+              <div className='thingy' key='react'>
+                react lover <img src='/static/react.svg' />
+              </div>,
+              <div className='thingy' key='monads'>
+                know about monads <img src='/static/haskell.svg' />
+              </div>,
+              <div className='thingy' key='sad'>
+                sad boy <img src='/static/sad-face.svg' />
+              </div>,
+              <div className='thingy' key='apple'>
+                happy  devices user
+              </div>,
+              <div className='thingy' key='phd'>
+                Ph.D. student <img src='/static/master.svg' />
+              </div>
+            ]).map(thingy => <Frame key={thingy} duration={5000}>{thingy}</Frame>)}
+          </Keyframes>
+        </div>
+      </NoSSR>
     </div>
 
     <style jsx>{`
@@ -37,6 +52,20 @@ export default () => (
 
       .container > div {
         margin: 5px 0;
+      }
+
+      .thingy-container {
+        max-height: 20px;
+      }
+
+      .thingy {
+        display: flex;
+        align-items: center;
+      }
+
+      .thingy > img {
+        height: 20px;
+        margin-left: 5px;
       }
     `}</style>
   </Page>
