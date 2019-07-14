697fb65dabc901d081e545f2f1a743b5beb2bcec
Zach Orlovsky <sadorlovsky@gmail.com>
2019-07-14 12:14:11 +0300

    v0.9.0

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,36 @@ export default () => (
 
     <main>
       <h1>Zach Orlovsky</h1>
+      <div className='social'>
+        <a href='https://github.com/sadorlovsky'>
+          <amp-img
+            width={32}
+            height={32}
+            srcSet='/static/github/GitHub-Mark-Light-120px-plus.png 120w, /static/github/GitHub-Mark-Light-64px.png 64w, /static/github/GitHub-Mark-Light-32px.png 32w'
+            src='/static/github/GitHub-Mark-Light-32px.png'
+            alt='https://github.com/sadorlovsky'
+            style={{ width: '32px' }}
+          />
+        </a>
+        <a href='https://twitter.com/sadorlovsky'>
+          <amp-img
+            width={32}
+            height={32}
+            src='/static/twitter/Twitter_Social_Icon_Circle_White.png'
+            alt='https://twitter.com/sadorlovsky'
+            style={{ width: '32px' }}
+          />
+        </a>
+        <a href='https://instagram.com/sadorlovsky'>
+          <amp-img
+            width={32}
+            height={32}
+            src='/static/instagram/glyph-logo_May2016.png'
+            alt='https://instagram.com/sadorlovsky'
+            style={{ width: '32px', filter: 'invert(100%)' }}
+          />
+        </a>
+      </div>
     </main>
 
     <style jsx global>{`
@@ -25,20 +55,26 @@ export default () => (
         padding: 0;
       }
       body {
-        background: #000;
+        background-color: #000;
+        color: #fff;
+        font-family: sans-serif;
       }
     `}</style>
     <style jsx>{`
       main {
         margin-top: 1em;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
       }
       h1 {
-        margin: 0;
+        margin: 0.5em 0;
         cursor: default;
-        color: pink;
-        font-family: sans-serif;
         text-align: center;
         font-weight: bold;
+      }
+      .social > a:not(:last-child) {
+        margin-right: 1em;
       }
     `}</style>
   </>
