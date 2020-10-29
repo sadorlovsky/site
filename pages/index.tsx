9c837db696ccd4eb73ee629727a9116fbf7021eb
Zach Orlovsky <sadorlovsky@gmail.com>
2020-10-29 03:16:43 +0300

    v0.11.0

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import { useState, useCallback, MouseEventHandler } from 'react'
-import Head from 'next/head'
 import { NextSeo } from 'next-seo'
 
 const IndexPage = () => {
@@ -37,13 +36,6 @@ const IndexPage = () => {
         title='Zach Orlovsky'
         description='Zach Orlovsky personal page'
       />
-
-      <Head>
-        <link rel='apple-touch-icon' sizes='180x180' href='/favicon/apple-touch-icon.png' />
-        <link rel='icon' type='image/png' sizes='32x32' href='/favicon/favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/favicon/favicon-16x16.png' />
-        <link rel='manifest' href='/favicon/site.webmanifest' />
-      </Head>
 
       <main onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
         <div>
