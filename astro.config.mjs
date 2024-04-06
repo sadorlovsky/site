2a4c0bb22aa26203ab0e88e76b49fbeb2e1f6d89
Zach Orlovsky <sadorlovsky@gmail.com>
2024-04-06 14:05:11 +0500

    astro + vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
+import vercelStatic from '@astrojs/vercel/static';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+  adapter: vercelStatic(),
+});
