22dc343609aed279900e6a685584b9dfdf9d27c4
Zach Orlovsky <sadorlovsky@gmail.com>
2024-04-07 15:20:03 +0500

    💅 layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,23 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/static";
+import icon from "astro-icon";
+import { rendererRich, transformerTwoslash } from '@shikijs/twoslash';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: vercel(),
+  integrations: [icon(), mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: 'catppuccin-macchiato',
+      transformers: [
+        transformerTwoslash({
+          renderer: rendererRich()
+        }),
+      ],
+    },
+  },
 });
