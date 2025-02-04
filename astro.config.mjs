965db5f0168d8b421681a8e34a8fc9c86cb9a489
Gogs <gogs@fake.local>
2025-02-04 06:41:31 +0500

    bun

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/static";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
@@ -7,7 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  adapter: vercel(),
   integrations: [icon(), mdx(), react()],
   markdown: {
     shikiConfig: {
